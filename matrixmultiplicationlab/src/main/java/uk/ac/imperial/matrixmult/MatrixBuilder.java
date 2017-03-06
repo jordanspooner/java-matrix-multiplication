fba138aef2eda9a736fcec44da4d7fd0d1babe5f
Konstantinos Gkoutzis <k.gkoutzis@imperial.ac.uk>
2017-03-06 09:05:42 +0000

    Original skeleton

---
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixBuilder.java
@@ -0,0 +1,19 @@
+package uk.ac.imperial.matrixmult;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class MatrixBuilder {
+
+
+  public static Matrix build(double[][] source) {
+    //TODO
+    return null;
+  }
+
+  public static Matrix build(int nRows, int nCols) {
+    //TODO
+    return null;
+  }
+
+}
---
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixMultiplier.java
@@ -0,0 +1,10 @@
+package uk.ac.imperial.matrixmult;
+
+public class MatrixMultiplier {
+
+  public static Matrix multiply(Matrix a, Matrix b) throws Exception {
+    //TODO
+    return null;
+  }
+  
+}
