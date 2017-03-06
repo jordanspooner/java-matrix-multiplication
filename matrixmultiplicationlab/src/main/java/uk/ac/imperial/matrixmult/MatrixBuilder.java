d74db8f15a61ce88024b2e45e05f53d3a063316f
Jordan Spooner <js4416@ic.ac.uk>
2017-03-06 19:50:16 +0000

    Initial non-concurrent implementation.

--- matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixBuilder.java
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixBuilder.java
@@ -5,15 +5,26 @@ import java.io.FileReader;
 
 public class MatrixBuilder {
 
-
   public static Matrix build(double[][] source) {
-    //TODO
-    return null;
+    int nRows = source.length;
+    int nCols = source[0].length;
+    Matrix matrix = new MatrixImplementation(nRows, nCols);
+    for (int row = 0; row < nRows; row++) {
+      for (int col = 0; col < nCols; col++) {
+        matrix.set(row, col, source[row][col]);
+      }
+    }
+    return matrix;
   }
 
   public static Matrix build(int nRows, int nCols) {
-    //TODO
-    return null;
+    Matrix matrix = new MatrixImplementation(nRows, nCols);
+    for (int row = 0; row < nRows; row++) {
+      for (int col = 0; col < nCols; col++) {
+        matrix.set(row, col, 0);
+      }
+    }
+    return matrix;
   }
 
 }
--- matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixMultiplier.java
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixMultiplier.java
@@ -3,8 +3,15 @@ package uk.ac.imperial.matrixmult;
 public class MatrixMultiplier {
 
   public static Matrix multiply(Matrix a, Matrix b) throws Exception {
-    //TODO
-    return null;
+    Matrix c = new MatrixImplementation(a.getNumRows(), b.getNumColumns());
+    for (int i = 0; i < a.getNumRows(); i ++) {
+      for (int j = 0; j < a.getNumColumns(); j ++) {
+        for (int k = 0; k < b.getNumColumns(); k ++) {
+          c.set(i, k, c.get(i,k) + a.get(i, j) * b.get(j, k));
+        }
+      }
+    }
+    return c;
   }
   
 }
