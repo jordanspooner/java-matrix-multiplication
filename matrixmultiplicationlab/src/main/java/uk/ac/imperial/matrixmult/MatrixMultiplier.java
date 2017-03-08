6dbd58d3a6924fbc32b5ad033a30b873a19538bf
Jordan Spooner <js4416@ic.ac.uk>
2017-03-08 14:45:25 +0000

    Major improvements

--- matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixMultiplier.java
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixMultiplier.java
@@ -1,10 +1,47 @@
 package uk.ac.imperial.matrixmult;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class MatrixMultiplier {
 
-  public static Matrix multiply(Matrix a, Matrix b) throws Exception {
-    //TODO
-    return null;
+//  // ****  NO MULTITHREADING **** //
+//
+//  public static Matrix multiply(Matrix a, Matrix b) throws Exception {
+//    Matrix c = new MatrixImplementation(a.getNumRows(), b.getNumColumns());
+//    for (int i = 0; i < a.getNumRows(); i ++) {
+//      for (int k = 0; k < a.getNumColumns(); k ++) {
+//        for (int j = 0; j < b.getNumColumns(); j ++) {
+//          c.set(i, j, c.get(i,j) + a.get(i, k) * b.get(k, j));
+//        }
+//      }
+//    }
+//    return c;
+//  }
+
+
+
+  // ****  MULTITHREADING NAIVE IMPLEMENTATION **** //
+
+  public static Matrix multiply(final Matrix a, final Matrix b) throws Exception {
+    Matrix c = new MatrixImplementation(a.getNumRows(), b.getNumColumns());
+    ExecutorService pool = Executors.newFixedThreadPool(10);
+    for (int row = 0; row < a.getNumRows(); row ++) {
+      MatrixMultiply thread = new MatrixMultiply(a, b, c, row);
+      pool.submit(thread);
+    }
+    pool.shutdown();
+    pool.awaitTermination(2, TimeUnit.MINUTES);
+    return c;
   }
-  
+
+
+
+//  // ****  MULTITHREADING STRASSEN'S IMPLEMENTATION **** //
+//
+//  public static Matrix multiply(final Matrix a, final Matrix b) throws Exception {
+//    //TODO
+//  }
+
 }
