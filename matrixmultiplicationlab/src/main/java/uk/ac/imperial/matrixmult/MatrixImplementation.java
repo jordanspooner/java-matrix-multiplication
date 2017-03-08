afefb6176484cfc98da33a5a49b1f3324425b10c
Jordan Spooner <js4416@ic.ac.uk>
2017-03-08 10:31:54 +0000

    Added very basic immplementation

---
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixImplementation.java
@@ -0,0 +1,28 @@
+package uk.ac.imperial.matrixmult;
+
+public class MatrixImplementation implements Matrix {
+
+  private double[][] matrix;
+
+  // Note that initialises to null values !
+  public MatrixImplementation(int rows, int columns) {
+    matrix = new double[rows][columns];
+  }
+
+  public double get(int row, int column) {
+    return matrix[row][column];
+  }
+
+  public void set(int row, int column, double value) {
+    matrix[row][column] = value;
+  }
+
+  public int getNumRows() {
+    return matrix.length;
+  }
+
+  public int getNumColumns() {
+    return matrix[0].length;
+  }
+
+}
