fa72e536498e9205f97c8bd2fbc85f58d9c85bf6
Jordan Spooner <js4416@ic.ac.uk>
2017-03-09 16:09:32 +0000

    Added runnable classes

---
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/ArrayMatrix.java
@@ -0,0 +1,80 @@
+package uk.ac.imperial.matrixmult;
+
+public class ArrayMatrix implements Matrix {
+
+  private double[][] matrix;
+  private int rowStartIndex;
+  private int rowEndIndex;
+  private int colStartIndex;
+  private int colEndIndex;
+
+  public ArrayMatrix(int rows, int columns) {
+    matrix = new double[rows][columns];
+    rowStartIndex = 0;
+    rowEndIndex = rows;
+    colStartIndex = 0;
+    colEndIndex = columns;
+  }
+
+  public ArrayMatrix(double[][] matrix, int rowStartIndex, int rowEndIndex, int colStartIndex, int colEndIndex) {
+    this.matrix = matrix;
+    this.rowStartIndex = rowStartIndex;
+    this.rowEndIndex = rowEndIndex;
+    this.colStartIndex = colStartIndex;
+    this.colEndIndex = colEndIndex;
+  }
+
+  @Override
+  public double get(int row, int column) {
+    return matrix[rowStartIndex + row][colStartIndex + column];
+  }
+
+  @Override
+  public void set(int row, int column, double value) {
+    matrix[rowStartIndex + row][colStartIndex + column] = value;
+  }
+
+  @Override
+  public int getNumRows() {
+    return rowEndIndex - rowStartIndex;
+  }
+
+  @Override
+  public int getNumColumns() {
+    return colEndIndex - colStartIndex;
+  }
+
+  @Override
+  public void add(Matrix a, Matrix b) {
+    int n = a.getNumRows();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        try {
+          set(i, j, a.get(i, j) + b.get(i, j));
+        } catch (IndexOutOfBoundsException e) {
+          break;
+        }
+      }
+    }
+  }
+
+  @Override
+  public void subtract(Matrix a, Matrix b) {
+    int n = a.getNumRows();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        try {
+          set(i, j, a.get(i, j) - b.get(i, j));
+        } catch (IndexOutOfBoundsException e) {
+          break;
+        }
+      }
+    }
+  }
+
+  @Override
+  public Matrix resizeTo(int rowStartIndex, int rowEndIndex, int colStartIndex, int colEndIndex) {
+    return new ArrayMatrix(matrix, rowStartIndex, rowEndIndex, colStartIndex, colEndIndex);
+  }
+
+}
