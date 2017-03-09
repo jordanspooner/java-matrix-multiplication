f2e97d7fb29ac8fec750ce3be86ddfd77a97cd1e
Jordan Spooner <js4416@ic.ac.uk>
2017-03-09 16:01:11 +0000

    All four methods implemented

--- matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/Matrix.java
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/Matrix.java
@@ -10,6 +10,12 @@ public interface Matrix {
 
   public int getNumColumns();
 
+  public void add(Matrix a, Matrix b);
+
+  public void subtract(Matrix a, Matrix b);
+
+  public Matrix resizeTo(int rowStartIndex, int rowEndIndex, int colStartIndex, int colEndIndex);
+
   public default boolean equals(Matrix m, double delta) {
     if ((m.getNumRows() != this.getNumRows())
         || (m.getNumColumns() != this.getNumColumns())) {
--- matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixBuilder.java
+++ matrixmultiplicationlab/src/main/java/uk/ac/imperial/matrixmult/MatrixBuilder.java
@@ -1,14 +1,11 @@
 package uk.ac.imperial.matrixmult;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class MatrixBuilder {
 
   public static Matrix build(double[][] source) {
     int nRows = source.length;
     int nCols = source[0].length;
-    Matrix matrix = new MatrixImplementation(nRows, nCols);
+    Matrix matrix = new ArrayMatrix(nRows, nCols);
     for (int row = 0; row < nRows; row++) {
       for (int col = 0; col < nCols; col++) {
         matrix.set(row, col, source[row][col]);
@@ -18,7 +15,7 @@ public class MatrixBuilder {
   }
 
   public static Matrix build(int nRows, int nCols) {
-    Matrix matrix = new MatrixImplementation(nRows, nCols);
+    Matrix matrix = new ArrayMatrix(nRows, nCols);
     for (int row = 0; row < nRows; row++) {
       for (int col = 0; col < nCols; col++) {
         matrix.set(row, col, 0);
